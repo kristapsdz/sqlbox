04f97731482e9b4ae9a5d35db72211f392682da7
kristapsdz <kristaps@bsd.lv>
2019-10-07 08:19:13 +0000

    More performance testing.

--- perf.sh
+++ perf.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
 TMPFILE=`mktemp` || exit 1
+TMPFILE2=`mktemp` || exit 1
 
-trap "rm -f $TMPFILE tmp.dat" EXIT ERR INT HUP QUIT
+trap "rm -f $TMPFILE $TMPFILE2" EXIT ERR INT HUP QUIT
 
 prefix="$1"
 
@@ -10,15 +11,15 @@ set -e
 
 echo '# n ksql sqlbox sqlite3'
 
-for f in 100 1000 10000 20000 40000
+for f in 500 1000 5000 10000 20000
 do
 	i=0
 	cat /dev/null >$TMPFILE
 	while [[ $i -lt 10 ]]
 	do
 		echo /usr/bin/time "./$prefix-ksql" -n $f 1>&2
-		/usr/bin/time "./$prefix-ksql" -n $f >/dev/null 2>tmp.dat
-		awk '{print $1}' tmp.dat >> $TMPFILE
+		/usr/bin/time "./$prefix-ksql" -n $f >/dev/null 2>$TMPFILE2
+		awk '{print $1}' $TMPFILE2 >> $TMPFILE
 		i=$(( $i + 1 ))
 	done
 	v1=`awk '{sum+=$1; sumsq+=$1*$1} END {print sum/NR, sqrt(sumsq/NR - (sum/NR)^2)}' $TMPFILE`
@@ -28,8 +29,8 @@ do
 	while [[ $i -lt 10 ]]
 	do
 		echo /usr/bin/time "./$prefix-sqlbox" -n $f 1>&2
-		/usr/bin/time "./$prefix-sqlbox" -n $f >/dev/null 2>tmp.dat
-		awk '{print $1}' tmp.dat >> $TMPFILE
+		/usr/bin/time "./$prefix-sqlbox" -n $f >/dev/null 2>$TMPFILE2
+		awk '{print $1}' $TMPFILE2 >> $TMPFILE
 		i=$(( $i + 1 ))
 	done
 	v2=`awk '{sum+=$1; sumsq+=$1*$1} END {print sum/NR, sqrt(sumsq/NR - (sum/NR)^2)}' $TMPFILE`
@@ -39,8 +40,8 @@ do
 	while [[ $i -lt 10 ]]
 	do
 		echo /usr/bin/time "./$prefix-sqlite3" -n $f 1>&2
-		/usr/bin/time "./$prefix-sqlite3" -n $f >/dev/null 2>tmp.dat
-		awk '{print $1}' tmp.dat >> $TMPFILE
+		/usr/bin/time "./$prefix-sqlite3" -n $f >/dev/null 2>$TMPFILE2
+		awk '{print $1}' $TMPFILE2 >> $TMPFILE
 		i=$(( $i + 1 ))
 	done
 	v3=`awk '{sum+=$1; sumsq+=$1*$1} END {print sum/NR, sqrt(sumsq/NR - (sum/NR)^2)}' $TMPFILE`
