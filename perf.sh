d0d681c5995bd45acf7e46fb238fe4d45c557014
kristapsdz <kristaps@bsd.lv>
2019-10-08 12:55:44 +0000

    Updated performance metrics.

--- perf.sh
+++ perf.sh
@@ -5,6 +5,7 @@ TMPFILE2=`mktemp` || exit 1
 
 trap "rm -f $TMPFILE $TMPFILE2" EXIT ERR INT HUP QUIT
 
+ITERS=20
 prefix="$1"
 
 set -e
@@ -15,7 +16,7 @@ for f in 500 1000 2000 4000 8000 16000
 do
 	i=0
 	cat /dev/null >$TMPFILE
-	while [[ $i -lt 10 ]]
+	while [[ $i -lt $ITERS ]]
 	do
 		echo /usr/bin/time "./$prefix-ksql" -n $f 1>&2
 		/usr/bin/time "./$prefix-ksql" -n $f >/dev/null 2>$TMPFILE2
@@ -26,7 +27,7 @@ do
 
 	i=0
 	cat /dev/null >$TMPFILE
-	while [[ $i -lt 10 ]]
+	while [[ $i -lt $ITERS ]]
 	do
 		echo /usr/bin/time "./$prefix-sqlbox" -n $f 1>&2
 		/usr/bin/time "./$prefix-sqlbox" -n $f >/dev/null 2>$TMPFILE2
@@ -37,7 +38,7 @@ do
 
 	i=0
 	cat /dev/null >$TMPFILE
-	while [[ $i -lt 10 ]]
+	while [[ $i -lt $ITERS ]]
 	do
 		echo /usr/bin/time "./$prefix-sqlite3" -n $f 1>&2
 		/usr/bin/time "./$prefix-sqlite3" -n $f >/dev/null 2>$TMPFILE2
